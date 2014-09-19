9032b390768afe8f1724f34e23a6accd03f42a4b
fracpete <fracpete@gmail.com>
2014-09-19 03:29:33 +0000

    interceptors that place data in queues when intercepting messages

---
+++ adams-webservice/src/main/java/adams/flow/webservice/interceptor/EnqueueOnOutgoingInterceptor.java
@@ -0,0 +1,167 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * EnqueueOnOutgoingInterceptor.java
+ * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ */
+package adams.flow.webservice.interceptor;
+
+import java.io.OutputStream;
+import java.io.Writer;
+
+import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.interceptor.LoggingMessage;
+import org.apache.cxf.io.CacheAndWriteOutputStream;
+import org.apache.cxf.message.Message;
+
+import adams.flow.control.StorageName;
+import adams.flow.control.StorageQueueHandler;
+import adams.flow.core.Actor;
+import adams.flow.core.NullToken;
+
+/**
+ * Enqueues a token in the specified queue whenever an outgoing message 
+ * is intercepted.
+ * 
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class EnqueueOnOutgoingInterceptor
+  extends AbstractInInterceptor
+  implements InterceptorWithActor<Message> {
+
+  /** the queue to enqueue the token in. */
+  protected StorageName m_StorageName;
+  
+  /** whether to enqueue the message or just a {@link NullToken}. */
+  protected boolean m_EnqueueMessage;
+  
+  /** the actor to use for getting access to queues. */
+  protected Actor m_Actor;
+  
+  /**
+   * Initializes the members.
+   */
+  @Override
+  protected void initialize() {
+    super.initialize();
+    
+    m_StorageName    = new StorageName("queue");
+    m_EnqueueMessage = false;
+    m_Actor          = null;
+  }
+  
+  /**
+   * Sets the queue to use.
+   * 
+   * @param value	the queue
+   */
+  public void setStorageName(StorageName value) {
+    m_StorageName = value;
+  }
+  
+  /**
+   * Returns the queue in use.
+   * 
+   * @return		the queue
+   */
+  public StorageName getStorageName() {
+    return m_StorageName;
+  }
+
+  /**
+   * Sets whether to enqueue the whole message or just a {@link NullToken}.
+   * 
+   * @param value	true if to enqueue whole message
+   */
+  public void setEnqueueMessage(boolean value) {
+    m_EnqueueMessage = value;
+  }
+  
+  /**
+   * Returns whether to enqueue the whole message or just a {@link NullToken}.
+   * 
+   * @return		true if to enqueue whole message
+   */
+  public boolean getEnqueueMessage() {
+    return m_EnqueueMessage;
+  }
+  
+  /**
+   * Sets the actor to use.
+   * 
+   * @param value	the actor to use
+   */
+  @Override
+  public void setActor(Actor value) {
+    m_Actor = value;
+  }
+
+  /**
+   * Returns the actor in use.
+   * 
+   * @return		the actor in use
+   */
+  @Override
+  public Actor getActor() {
+    return m_Actor;
+  }
+  
+  /**
+   * Intercepts a message. 
+   * Interceptors should NOT invoke handleMessage or handleFault
+   * on the next interceptor - the interceptor chain will
+   * take care of this.
+   * 
+   * @param message
+   */
+  @Override
+  public void handleMessage(Message message) throws Fault {
+    final OutputStream 	os;
+    final Writer 	iowriter;
+    boolean 		hasLogged;
+    
+    if (m_Actor == null)
+      return;
+
+    // Write the output while caching it for the log message
+    hasLogged = message.containsKey(InterceptorHelper.OUTGOING_LOG_SETUP);
+    if (hasLogged)
+      return;
+    
+    if (m_EnqueueMessage) {
+      os       = message.getContent(OutputStream.class);
+      iowriter = message.getContent(Writer.class);
+      if ((os == null) && (iowriter == null))
+        return;
+
+      message.put(InterceptorHelper.OUTGOING_LOG_SETUP, Boolean.TRUE);
+      if (os != null) {
+	final CacheAndWriteOutputStream newOut = new CacheAndWriteOutputStream(os);
+	message.setContent(OutputStream.class, newOut);
+	newOut.registerCallback(new AbstractOutgoingCallback(message, os) {
+	  @Override
+	  protected void write(LoggingMessage buffer) {
+	    ((StorageQueueHandler) m_Actor.getStorageHandler().getStorage().get(m_StorageName)).add("" + buffer);
+	  }
+	});
+      }
+    }
+    else {
+      ((StorageQueueHandler) m_Actor.getStorageHandler().getStorage().get(m_StorageName)).add(new NullToken());
+    }
+  }
+}
---
+++ adams-webservice/src/main/java/adams/flow/webservice/interceptor/EnqueueOnOutgoingInterceptorGenerator.java
@@ -0,0 +1,142 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * EnqueueOnOutgoingInterceptorGenerator.java
+ * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ */
+package adams.flow.webservice.interceptor;
+
+import adams.flow.control.StorageName;
+import adams.flow.core.NullToken;
+
+/**
+ * Generator for {@link EnqueueOnOutgoingInterceptor}.
+ * 
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class EnqueueOnOutgoingInterceptorGenerator
+  extends AbstractInInterceptorGenerator<EnqueueOnOutgoingInterceptor> {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -8109018608359183466L;
+
+  /** the queue to enqueue the token in. */
+  protected StorageName m_StorageName;
+  
+  /** whether to enqueue the message or just a {@link NullToken}. */
+  protected boolean m_EnqueueMessage;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Generates an " + EnqueueOnOutgoingInterceptor.class.getName() + " instance.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+	    "storage-name", "storageName",
+	    new StorageName("queue"));
+
+    m_OptionManager.add(
+	    "enqueue-message", "enqueueMessage",
+	    false);
+  }
+
+  /**
+   * Sets the queue to use.
+   * 
+   * @param value	the queue
+   */
+  public void setStorageName(StorageName value) {
+    m_StorageName = value;
+    reset();
+  }
+  
+  /**
+   * Returns the queue in use.
+   * 
+   * @return		the queue
+   */
+  public StorageName getStorageName() {
+    return m_StorageName;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String storageNameTipText() {
+    return "The name of the queue to send data to.";
+  }
+
+  /**
+   * Sets whether to enqueue the whole message or just a {@link NullToken}.
+   * 
+   * @param value	true if to enqueue whole message
+   */
+  public void setEnqueueMessage(boolean value) {
+    m_EnqueueMessage = value;
+    reset();
+  }
+  
+  /**
+   * Returns whether to enqueue the whole message or just a {@link NullToken}.
+   * 
+   * @return		true if to enqueue whole message
+   */
+  public boolean getEnqueueMessage() {
+    return m_EnqueueMessage;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String enqueueMessageTipText() {
+    return "If enabled, the complete message gets enqueued, otherwise just a " + NullToken.class.getName() + ".";
+  }
+
+  /**
+   * Generates the actual interceptor for incoming messages.
+   * 
+   * @return		the interceptor
+   */
+  @Override
+  protected EnqueueOnOutgoingInterceptor doGenerate() {
+    EnqueueOnOutgoingInterceptor	result;
+    
+    result = new EnqueueOnOutgoingInterceptor();
+    result.setStorageName(getStorageName());
+    result.setEnqueueMessage(getEnqueueMessage());
+    
+    return result;
+  }
+}
