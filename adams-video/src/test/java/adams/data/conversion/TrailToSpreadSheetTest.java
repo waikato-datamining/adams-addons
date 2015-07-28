37d6b184aa308d130b526c30b2db8c0a82c1e755
fracpete <fracpete@gmail.com>
2015-07-28 02:56:15 +0000

    added conversion unit test

---
+++ adams-video/src/test/java/adams/data/conversion/TrailToSpreadSheetTest.java
@@ -0,0 +1,127 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * TrailToSpreadSheet.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.data.conversion;
+
+import adams.data.io.input.SimpleTrailReader;
+import adams.data.trail.Trail;
+import adams.env.Environment;
+import adams.test.TmpFile;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests the TrailToSpreadSheet conversion.
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class TrailToSpreadSheetTest
+  extends AbstractConversionTestCase {
+
+  /**
+   * Constructs the test case. Called by subclasses.
+   *
+   * @param name 	the name of the test
+   */
+  public TrailToSpreadSheetTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception if an error occurs
+   */
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    m_TestHelper.copyResourceToTmp("mouse1.trail");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  @Override
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("mouse1.trail");
+    super.tearDown();
+  }
+
+  /**
+   * Returns the input data to use in the regression test.
+   *
+   * @return		the objects
+   */
+  @Override
+  protected Object[] getRegressionInput() {
+    SimpleTrailReader 	reader;
+    Trail 		trail;
+
+    reader = new SimpleTrailReader();
+    reader.setInput(new TmpFile("mouse1.trail"));
+    trail = reader.read().get(0);
+
+    return new Trail[]{trail};
+  }
+
+  /**
+   * Returns the setups to use in the regression test.
+   *
+   * @return		the setups
+   */
+  @Override
+  protected Conversion[] getRegressionSetups() {
+    return new Conversion[]{
+	new TrailToSpreadSheet()
+    };
+  }
+
+  /**
+   * Returns the ignored line indices to use in the regression test.
+   *
+   * @return		the setups
+   */
+  @Override
+  protected int[] getRegressionIgnoredLineIndices() {
+    return new int[0];
+  }
+
+  /**
+   * Returns the test suite.
+   *
+   * @return		the suite
+   */
+  public static Test suite() {
+    return new TestSuite(TrailToSpreadSheetTest.class);
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
