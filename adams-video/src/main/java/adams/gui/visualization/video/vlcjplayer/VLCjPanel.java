2f12da8ccd3f9bf574b99ab4c9c12d5acf1e3f9c
fracpete <fracpete@gmail.com>
2016-01-12 20:15:08 +0000

    mute button changes icon/text now

--- adams-video/src/main/java/adams/gui/visualization/video/vlcjplayer/VLCjPanel.java
+++ adams-video/src/main/java/adams/gui/visualization/video/vlcjplayer/VLCjPanel.java
@@ -37,8 +37,16 @@ import adams.gui.event.RecentItemListener;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
 import uk.co.caprica.vlcj.discovery.NativeDiscovery;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JSlider;
+import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -743,6 +751,15 @@ public class VLCjPanel
 	m_PlayButton.setAction(m_PauseAction);
       } else {
 	m_PlayButton.setAction(m_PlayAction);
+      }
+
+      if (m_MediaPlayerComponent.getMediaPlayer().isMute()) {
+        m_MuteButton.setText("Unmute");
+        m_MuteButton.setIcon(GUIHelper.getIcon("unmute.png"));
+      }
+      else {
+        m_MuteButton.setText("Mute");
+        m_MuteButton.setIcon(GUIHelper.getIcon("mute.png"));
       }
 
       // Enables or disables the slider
