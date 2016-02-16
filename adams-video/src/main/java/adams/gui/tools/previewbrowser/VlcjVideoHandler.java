7b063e106117d052ab6091c6b72ec46528bfdc63
fracpete <fracpete@gmail.com>
2016-02-16 04:28:39 +0000

    preview handler for videos

---
+++ adams-video/src/main/java/adams/gui/tools/previewbrowser/VlcjVideoHandler.java
@@ -0,0 +1,88 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * VlcjVideoHandler.java
+ * Copyright (C) 2016 University of Waikato, Hamilton, New Zealand
+ */
+package adams.gui.tools.previewbrowser;
+
+import adams.core.Utils;
+import adams.gui.visualization.video.vlcjplayer.VLCjDirectRenderPanel;
+
+import java.io.File;
+
+/**
+ <!-- globalinfo-start -->
+ * Displays the following video files using VLCj: 3gp,avi,mov,mp4,mpeg,mpg,m2ts,mkv,mts,ogv,ts,vob,webm,wmv
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class VlcjVideoHandler
+  extends AbstractContentHandler {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 639887373783415695L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Displays the following video files using VLCj: " + Utils.arrayToString(getExtensions());
+  }
+
+  /**
+   * Returns the list of extensions (without dot) that this handler can
+   * take care of.
+   *
+   * @return		the list of extensions (no dot)
+   */
+  @Override
+  public String[] getExtensions() {
+    return new String[]{
+      "3gp", "avi", "mov", "mp4", "mpeg", "mpg", "m2ts", "mkv", "mts",
+      "ogv", "ts", "vob", "webm", "wmv"};
+  }
+
+  /**
+   * Creates the actual view.
+   *
+   * @param file	the file to create the view for
+   * @return		the view
+   */
+  @Override
+  protected PreviewPanel createPreview(File file) {
+    VLCjDirectRenderPanel 	panel;
+
+    panel = new VLCjDirectRenderPanel();
+    panel.open(file);
+
+    return new PreviewPanel(panel, panel);
+  }
+}
