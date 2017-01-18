0c30bd9dc08f401daa1b0d6f6e5526a6daa2aeee
fracpete <fracpete@gmail.com>
2017-01-18 20:40:49 +0000

    added cross-validation support

---
+++ adams-dl4j/src/main/java/adams/flow/transformer/DL4JCrossValidationEvaluator.java
@@ -0,0 +1,334 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * DL4JCrossValidationEvaluator.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.transformer;
+
+import adams.core.QuickInfoHelper;
+import adams.core.Randomizable;
+import adams.flow.container.DL4JEvaluationContainer;
+import adams.flow.core.Token;
+import adams.flow.provenance.ActorType;
+import adams.flow.provenance.Provenance;
+import adams.flow.provenance.ProvenanceContainer;
+import adams.flow.provenance.ProvenanceInformation;
+import adams.flow.provenance.ProvenanceSupporter;
+import adams.ml.dl4j.model.ModelConfigurator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.api.Layer;
+import org.deeplearning4j.nn.api.Model;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.KFoldIterator;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.Random;
+
+/**
+ <!-- globalinfo-start -->
+ * Cross-validates a model on the incoming dataset.<br>
+ * The model setup being used in the evaluation is obtained from the callable actor returning a model configurator.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;org.nd4j.linalg.dataset.DataSet<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;java.lang.String<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: DL4JCrossValidationEvaluator
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-always-use-container &lt;boolean&gt; (property: alwaysUseContainer)
+ * &nbsp;&nbsp;&nbsp;If enabled, always outputs an evaluation container.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-model &lt;adams.flow.core.CallableActorReference&gt; (property: model)
+ * &nbsp;&nbsp;&nbsp;The callable model configurator actor to obtain the model from to train 
+ * &nbsp;&nbsp;&nbsp;and evaluate on the test data.
+ * &nbsp;&nbsp;&nbsp;default: DL4JModelConfigurator
+ * </pre>
+ * 
+ * <pre>-seed &lt;long&gt; (property: seed)
+ * &nbsp;&nbsp;&nbsp;The seed value for the randomization.
+ * &nbsp;&nbsp;&nbsp;default: 1
+ * </pre>
+ * 
+ * <pre>-folds &lt;int&gt; (property: folds)
+ * &nbsp;&nbsp;&nbsp;The folds to use.
+ * &nbsp;&nbsp;&nbsp;default: 10
+ * &nbsp;&nbsp;&nbsp;minimum: 2
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class DL4JCrossValidationEvaluator
+  extends AbstractCallableDL4JModelEvaluator
+  implements Randomizable, ProvenanceSupporter {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -1092101024095887007L;
+
+  /** the seed value. */
+  protected long m_Seed;
+
+  /** the number of folds to generate. */
+  protected int m_Folds;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return
+      "Cross-validates a model on the incoming dataset.\n"
+        + "The model setup being used in the evaluation is obtained from the "
+        + "callable actor returning a model configurator.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "seed", "seed",
+      1L);
+
+    m_OptionManager.add(
+      "folds", "folds",
+      10, 2, null);
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    String	result;
+
+    result = super.getQuickInfo();
+    result += QuickInfoHelper.toString(this, "seed", m_Seed, ", seed: ");
+    result += QuickInfoHelper.toString(this, "folds", m_Folds, ", folds: ");
+
+    return result;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  @Override
+  public String modelTipText() {
+    return "The callable model configurator actor to obtain the model from to train and evaluate on the test data.";
+  }
+
+  /**
+   * Sets the seed value.
+   *
+   * @param value	the seed
+   */
+  public void setSeed(long value) {
+    m_Seed = value;
+    reset();
+  }
+
+  /**
+   * Returns the seed value.
+   *
+   * @return  		the seed
+   */
+  public long getSeed() {
+    return m_Seed;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String seedTipText() {
+    return "The seed value for the randomization.";
+  }
+
+  /**
+   * Sets the number of folds to use.
+   *
+   * @param value	the folds, use -1 for LOOCV
+   */
+  public void setFolds(int value) {
+    if (getOptionManager().isValid("folds", value)) {
+      m_Folds = value;
+      reset();
+    }
+  }
+
+  /**
+   * Returns the number of folds to generate.
+   *
+   * @return  		the folds, 1 in case of LOOCV
+   */
+  public int getFolds() {
+    return m_Folds;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String foldsTipText() {
+    return "The folds to use.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		<!-- flow-accepts-start -->org.nd4j.linalg.dataset.DataSet.class<!-- flow-accepts-end -->
+   */
+  public Class[] accepts() {
+    return new Class[]{DataSet.class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String			result;
+    DataSet 			data;
+    DataSet 			train;
+    DataSet 			test;
+    ModelConfigurator 		conf;
+    Model			model;
+    Evaluation 			eval;
+    KFoldIterator		iter;
+
+    result = null;
+
+    try {
+      conf = getModelConfiguratorInstance();
+      if (conf == null)
+	throw new IllegalStateException("Model configurator '" + getModel() + "' not found!");
+
+      data = (DataSet) m_InputToken.getPayload();
+      Nd4j.shuffle(data.getFeatureMatrix(), new Random(m_Seed), 1);
+      if (data.getLabels() != null)
+	Nd4j.shuffle(data.getLabels(), new Random(m_Seed), 1);
+
+      if (!(conf.configureModel(data.numInputs(), data.numOutcomes()) instanceof MultiLayerNetwork))
+        result = "Can only evaluate " + MultiLayerNetwork.class.getName() + "!";
+
+      eval = null;
+      if (result == null) {
+	eval = new Evaluation(data.numOutcomes());
+	iter = new KFoldIterator(m_Folds, data);
+	while (iter.hasNext()) {
+	  train = iter.next();
+	  test  = iter.testFold();
+	  model = conf.configureModel(data.numInputs(), data.numOutcomes());
+	  ((MultiLayerNetwork) model).fit(train);
+	  eval.eval(test.getLabels(), ((MultiLayerNetwork) model).output(test.getFeatureMatrix(), Layer.TrainingMode.TEST));
+	}
+      }
+
+      // broadcast result
+      if (eval != null) {
+	if (m_AlwaysUseContainer)
+	  m_OutputToken = new Token(new DL4JEvaluationContainer(eval, conf));
+	else
+	  m_OutputToken = new Token(eval.stats());
+      }
+    }
+    catch (Exception e) {
+      m_OutputToken = null;
+      result = handleException("Failed to evaluate: ", e);
+    }
+
+    if (m_OutputToken != null)
+      updateProvenance(m_OutputToken);
+
+    return result;
+  }
+
+  /**
+   * Updates the provenance information in the provided container.
+   *
+   * @param cont	the provenance container to update
+   */
+  public void updateProvenance(ProvenanceContainer cont) {
+    if (Provenance.getSingleton().isEnabled()) {
+      if (m_InputToken.hasProvenance())
+	cont.setProvenance(m_InputToken.getProvenance().getClone());
+      cont.addProvenance(new ProvenanceInformation(ActorType.EVALUATOR, m_InputToken.getPayload().getClass(), this, m_OutputToken.getPayload().getClass()));
+    }
+  }
+}
