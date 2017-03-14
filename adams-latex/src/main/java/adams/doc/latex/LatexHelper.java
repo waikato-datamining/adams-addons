1523b17ab055e96fee6cd85396b2f33fb09f8027
fracpete <fracpete@gmail.com>
2017-03-14 21:53:11 +0000

    added LaTeX preferences

---
+++ adams-latex/src/main/java/adams/doc/latex/LatexHelper.java
@@ -0,0 +1,105 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * LatexHelper.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package adams.doc.latex;
+
+import adams.core.Properties;
+import adams.core.io.PlaceholderDirectory;
+import adams.core.management.OS;
+import adams.env.Environment;
+import adams.env.LatexDefinition;
+
+/**
+ * Helper class for LaTeX setup.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class LatexHelper {
+
+  /** the name of the props file. */
+  public final static String FILENAME = "LaTeX.props";
+
+  /** the binaries dir. */
+  public final static String BINARIES_DIR = "BinariesDir";
+
+  /** the properties. */
+  protected static Properties m_Properties;
+
+  /**
+   * Returns the underlying properties.
+   *
+   * @return		the properties
+   */
+  public synchronized static Properties getProperties() {
+    if (m_Properties == null) {
+      try {
+	m_Properties = Environment.getInstance().read(LatexDefinition.KEY);
+      }
+      catch (Exception e) {
+	m_Properties = new Properties();
+      }
+    }
+
+    return m_Properties;
+  }
+
+  /**
+   * Writes the specified properties to disk.
+   *
+   * @return		true if successfully stored
+   */
+  public synchronized static boolean writeProperties() {
+    return writeProperties(getProperties());
+  }
+
+  /**
+   * Writes the specified properties to disk.
+   *
+   * @param props	the properties to write to disk
+   * @return		true if successfully stored
+   */
+  public synchronized static boolean writeProperties(Properties props) {
+    boolean	result;
+
+    result = Environment.getInstance().write(LatexDefinition.KEY, props);
+    // require reload
+    m_Properties = null;
+
+    return result;
+  }
+
+  /**
+   * Returns the binaries dir.
+   *
+   * @return		the binaries dir
+   */
+  public static PlaceholderDirectory getBinariesDir() {
+    String	defPath;
+
+    if (OS.isLinux())
+      defPath = "/usr/bin";
+    else
+      defPath = ".";
+
+    return new PlaceholderDirectory(getProperties().getPath(BINARIES_DIR, defPath));
+  }
+
+}
---
+++ adams-latex/src/main/java/adams/gui/application/LatexSetupPanel.java
@@ -0,0 +1,146 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * LatexSetupPanel.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
+ */
+package adams.gui.application;
+
+import adams.core.Properties;
+import adams.core.io.FileUtils;
+import adams.doc.latex.LatexHelper;
+import adams.env.Environment;
+import adams.env.LatexDefinition;
+import adams.gui.chooser.DirectoryChooserPanel;
+import adams.gui.core.ParameterPanel;
+
+import java.awt.BorderLayout;
+
+/**
+ * Panel for configuring the system-wide LaTeX settings.
+ *
+ * @author  fracpete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class LatexSetupPanel
+  extends AbstractPreferencesPanel {
+
+  /** for serialization. */
+  private static final long serialVersionUID = -7937644706618374284L;
+
+  /** the parameters. */
+  protected ParameterPanel m_PanelParameters;
+
+  /** the consumer key. */
+  protected DirectoryChooserPanel m_ChooserLatexBinaries;
+
+  /**
+   * Initializes the members.
+   */
+  @Override
+  protected void initGUI() {
+    super.initGUI();
+
+    setLayout(new BorderLayout());
+
+    m_PanelParameters = new ParameterPanel();
+    add(m_PanelParameters, BorderLayout.CENTER);
+
+    m_ChooserLatexBinaries = new DirectoryChooserPanel();
+    m_ChooserLatexBinaries.setCurrent(LatexHelper.getBinariesDir());
+    m_PanelParameters.addParameter("_Binaries dir", m_ChooserLatexBinaries);
+  }
+
+  /**
+   * Turns the parameters in the GUI into a properties object.
+   *
+   * @return		the properties
+   */
+  protected Properties toProperties() {
+    Properties	result;
+
+    result = new Properties();
+
+    result.setProperty(LatexHelper.BINARIES_DIR, m_ChooserLatexBinaries.getCurrent().getAbsolutePath());
+
+    return result;
+  }
+
+  /**
+   * The title of the preference panel.
+   * 
+   * @return		the title
+   */
+  @Override
+  public String getTitle() {
+    return "Latex";
+  }
+
+  /**
+   * Returns whether the panel requires a wrapper scrollpane/panel for display.
+   * 
+   * @return		true if wrapper required
+   */
+  @Override
+  public boolean requiresWrapper() {
+    return true;
+  }
+  
+  /**
+   * Activates the twitter setup.
+   * 
+   * @return		null if successfully activated, otherwise error message
+   */
+  @Override
+  public String activate() {
+    boolean	result;
+
+    result = LatexHelper.writeProperties(toProperties());
+    if (result)
+      return null;
+    else
+      return "Failed to save LaTeX setup to " + LatexHelper.FILENAME + "!";
+  }
+
+  /**
+   * Returns whether the panel supports resetting the options.
+   *
+   * @return		true if supported
+   */
+  public boolean canReset() {
+    String	props;
+
+    props = Environment.getInstance().getCustomPropertiesFilename(LatexDefinition.KEY);
+    return (props != null) && FileUtils.fileExists(props);
+  }
+
+  /**
+   * Resets the settings to their default.
+   *
+   * @return		null if successfully reset, otherwise error message
+   */
+  public String reset() {
+    String	props;
+
+    props = Environment.getInstance().getCustomPropertiesFilename(LatexDefinition.KEY);
+    if ((props != null) && FileUtils.fileExists(props)) {
+      if (!FileUtils.delete(props))
+	return "Failed to remove custom Latex properties: " + props;
+    }
+
+    return null;
+  }
+}
