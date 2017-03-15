44d2689bb664e48e6ad6cbeaf1d321fd873ce565
fracpete <fracpete@gmail.com>
2017-03-15 01:15:38 +0000

    generators for TOC, list of figures/tables

---
+++ adams-latex/src/main/java/adams/doc/latex/generator/ListOfFigures.java
@@ -0,0 +1,74 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * ListOfFigures.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package adams.doc.latex.generator;
+
+/**
+ <!-- globalinfo-start -->
+ * Inserts the statement for the list of figures.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If enabled, the code generation gets skipped.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-no-var-expansion &lt;boolean&gt; (property: noVariableExpansion)
+ * &nbsp;&nbsp;&nbsp;If enabled, variable expansion gets skipped.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class ListOfFigures
+  extends AbstractCodeGenerator {
+
+  private static final long serialVersionUID = 6302309960098109973L;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Inserts the statement for the list of figures.";
+  }
+
+  /**
+   * Generates the actual code.
+   *
+   * @return		the generated code
+   */
+  @Override
+  protected String doGenerate() {
+    return "\\listoffigures\n";
+  }
+}
