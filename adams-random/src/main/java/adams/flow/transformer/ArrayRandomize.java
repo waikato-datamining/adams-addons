111fea12f4d744ed5cc055c30ff7d67f682c0089
fracpete <fracpete@gmail.com>
2017-03-16 23:50:28 +0000

    ArrayRandomize transformer randomizes copy of array

---
+++ adams-random/src/main/java/adams/flow/transformer/ArrayRandomize.java
@@ -0,0 +1,214 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * ArrayRandomize.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.transformer;
+
+import adams.core.ObjectCopyHelper;
+import adams.core.Randomizable;
+import adams.flow.core.Token;
+import adams.flow.core.Unknown;
+import gnu.trove.list.array.TIntArrayList;
+
+import java.lang.reflect.Array;
+import java.util.Random;
+
+/**
+ <!-- globalinfo-start -->
+ * Randomizes an array using a copy.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- flow-summary-start -->
+ * Input&#47;output:<br>
+ * - accepts:<br>
+ * &nbsp;&nbsp;&nbsp;adams.flow.core.Unknown[]<br>
+ * - generates:<br>
+ * &nbsp;&nbsp;&nbsp;adams.flow.core.Unknown[]<br>
+ * <br><br>
+ <!-- flow-summary-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: ArrayRandomize
+ * </pre>
+ * 
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default: 
+ * </pre>
+ * 
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this 
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical 
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing 
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ * <pre>-seed &lt;long&gt; (property: seed)
+ * &nbsp;&nbsp;&nbsp;The seed value for the randomization.
+ * &nbsp;&nbsp;&nbsp;default: 1
+ * </pre>
+ * 
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class ArrayRandomize
+  extends AbstractTransformer
+  implements Randomizable {
+
+  private static final long serialVersionUID = 4439862057128462481L;
+
+  /** the seed value. */
+  protected long m_Seed;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Randomizes an array using a copy.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "seed", "seed",
+      1L);
+  }
+
+  /**
+   * Sets the seed value.
+   *
+   * @param value	the seed
+   */
+  public void setSeed(long value) {
+    m_Seed = value;
+  }
+
+  /**
+   * Returns the seed value.
+   *
+   * @return  		the seed
+   */
+  public long getSeed() {
+    return m_Seed;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String seedTipText() {
+    return "The seed value for the randomization.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the Class of objects that can be processed
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{Unknown[].class};
+  }
+
+  /**
+   * Returns the class of objects that it generates.
+   *
+   * @return		the Class of the generated tokens
+   */
+  @Override
+  public Class[] generates() {
+    return new Class[]{Unknown[].class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String		result;
+    Object		array;
+    Object		copy;
+    TIntArrayList	indices;
+    int			i;
+    int			len;
+
+    result = null;
+
+    array = m_InputToken.getPayload();
+    copy  = null;
+    if (!array.getClass().isArray())
+      result = "Input is not an array!";
+
+    if (result == null) {
+      try {
+	copy = ObjectCopyHelper.copyObject(array);
+      }
+      catch (Exception e) {
+	result = handleException("Failed to create copy of array!", e);
+      }
+    }
+
+    if (result == null) {
+      len     = Array.getLength(array);
+      indices = new TIntArrayList(len);
+      for (i = 0; i < len; i++)
+	indices.add(i);
+      indices.shuffle(new Random(m_Seed));
+      for (i = 0; i < len; i++)
+	Array.set(copy, i, Array.get(array, indices.get(i)));
+      m_OutputToken = new Token(copy);
+    }
+
+    return result;
+  }
+}
---
+++ adams-random/src/test/java/adams/flow/transformer/ArrayRandomizeTest.java
@@ -0,0 +1,169 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ArraySubSampleTest.java
+ * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
+ */
+
+package adams.flow.transformer;
+
+import adams.core.option.AbstractArgumentOption;
+import adams.env.Environment;
+import adams.flow.AbstractFlowTest;
+import adams.flow.control.Flow;
+import adams.flow.core.Actor;
+import adams.test.TmpFile;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Test for ArraySubSample actor.
+ *
+ * @author fracpete
+ * @author adams.core.option.FlowJUnitTestProducer (code generator)
+ * @version $Revision$
+ */
+public class ArrayRandomizeTest
+  extends AbstractFlowTest {
+
+  /**
+   * Initializes the test.
+   *
+   * @param name	the name of the test
+   */
+  public ArrayRandomizeTest(String name) {
+    super(name);
+  }
+
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception 	if an error occurs.
+   */
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    m_TestHelper.copyResourceToTmp("numbers.txt");
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  @Override
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("numbers.txt");
+    m_TestHelper.deleteFileFromTmp("dumpfile.txt");
+
+    super.tearDown();
+  }
+
+  /**
+   * Performs a regression test, comparing against previously generated output.
+   */
+  public void testRegression() {
+    performRegressionTest(
+        new TmpFile[]{
+          new TmpFile("dumpfile.txt")
+        });
+  }
+
+  /**
+   *
+   * Returns a test suite.
+   *
+   * @return		the test suite
+   */
+  public static Test suite() {
+    return new TestSuite(ArrayRandomizeTest.class);
+  }
+
+  /**
+   * Used to create an instance of a specific actor.
+   *
+   * @return a suitably configured <code>Actor</code> value
+   */
+  @Override
+  public Actor getActor() {
+    AbstractArgumentOption    argOption;
+
+    Flow flow = new Flow();
+
+    try {
+      argOption = (AbstractArgumentOption) flow.getOptionManager().findByProperty("actors");
+      Actor[] actors1 = new Actor[5];
+
+      // Flow.FileSupplier
+      adams.flow.source.FileSupplier filesupplier2 = new adams.flow.source.FileSupplier();
+      argOption = (AbstractArgumentOption) filesupplier2.getOptionManager().findByProperty("files");
+      adams.core.io.PlaceholderFile[] files3 = new adams.core.io.PlaceholderFile[1];
+      files3[0] = (adams.core.io.PlaceholderFile) argOption.valueOf("${TMP}/numbers.txt");
+      filesupplier2.setFiles(files3);
+      actors1[0] = filesupplier2;
+
+      // Flow.TextFileReader
+      TextFileReader textfilereader4 = new TextFileReader();
+      argOption = (AbstractArgumentOption) textfilereader4.getOptionManager().findByProperty("reader");
+      adams.data.io.input.LineArrayTextReader linearraytextreader6 = new adams.data.io.input.LineArrayTextReader();
+      textfilereader4.setReader(linearraytextreader6);
+
+      actors1[1] = textfilereader4;
+
+      // Flow.ArrayRandomize
+      ArrayRandomize arrayrandomize7 = new ArrayRandomize();
+      arrayrandomize7.setSeed(1);
+
+      actors1[2] = arrayrandomize7;
+
+      // Flow.ArrayToSequence
+      ArrayToSequence arraytosequence11 = new ArrayToSequence();
+      actors1[3] = arraytosequence11;
+
+      // Flow.DumpFile
+      adams.flow.sink.DumpFile dumpfile12 = new adams.flow.sink.DumpFile();
+      argOption = (AbstractArgumentOption) dumpfile12.getOptionManager().findByProperty("outputFile");
+      dumpfile12.setOutputFile((adams.core.io.PlaceholderFile) argOption.valueOf("${TMP}/dumpfile.txt"));
+      dumpfile12.setAppend(true);
+
+      actors1[4] = dumpfile12;
+      flow.setActors(actors1);
+
+      argOption = (AbstractArgumentOption) flow.getOptionManager().findByProperty("flowExecutionListener");
+      adams.flow.execution.NullListener nulllistener15 = new adams.flow.execution.NullListener();
+      flow.setFlowExecutionListener(nulllistener15);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+
+    return flow;
+  }
+
+  /**
+   * Runs the test from commandline.
+   *
+   * @param args	ignored
+   */
+  public static void main(String[] args) {
+    Environment.setEnvironmentClass(Environment.class);
+    runTest(suite());
+  }
+}
+
