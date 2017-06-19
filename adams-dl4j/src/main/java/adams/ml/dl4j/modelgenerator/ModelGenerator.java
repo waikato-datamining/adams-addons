feaca706733b0d8343621af1ff6e53f3dd3ed4c5
fracpete <fracpete@gmail.com>
2017-06-19 22:14:56 +0000

    DL4JModelGenerator source generates model(s) using the specified generator scheme

---
+++ adams-dl4j/src/main/java/adams/ml/dl4j/modelgenerator/ModelGenerator.java
@@ -0,0 +1,45 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * ModelGenerator.java
+ * Copyright (C) 2017 University of Waikato, Hamilton, NZ
+ */
+
+package adams.ml.dl4j.modelgenerator;
+
+import adams.flow.core.FlowContextHandler;
+import org.deeplearning4j.nn.api.Model;
+
+import java.util.List;
+
+/**
+ * Interface for classes that generate DL4J models.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public interface ModelGenerator
+  extends FlowContextHandler {
+
+  /**
+   * Generates models.
+   *
+   * @param numInput	the number of input nodes
+   * @param numOutput	the number of output nodes
+   * @return		the models
+   */
+  public List<Model> generate(int numInput, int numOutput);
+}
