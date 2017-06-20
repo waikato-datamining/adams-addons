4f4f6a422de9c2f02ab98861a8382ac556f0ab6b
fracpete <fracpete@gmail.com>
2017-06-20 03:24:31 +0000

    actual unit test

--- adams-dl4j/src/test/java/adams/flow/sink/DL4JModelWriterTest.java
+++ adams-dl4j/src/test/java/adams/flow/sink/DL4JModelWriterTest.java
@@ -15,29 +15,38 @@
 
 /*
  * DL4JModelWriterTest.java
- * Copyright (C) 2016 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2017 University of Waikato, Hamilton, New Zealand
  */
 
 package adams.flow.sink;
 
+import adams.core.io.PlaceholderFile;
+import adams.core.option.AbstractArgumentOption;
+import adams.data.conversion.DL4JConfiguratorToModel;
 import adams.env.Environment;
-import adams.flow.AbstractDL4JFlowTest;
+import adams.flow.AbstractFlowTest;
 import adams.flow.control.Flow;
 import adams.flow.core.AbstractActor;
+import adams.flow.core.Actor;
+import adams.flow.execution.NullListener;
+import adams.flow.source.DL4JModelConfigurator;
+import adams.flow.transformer.Convert;
+import adams.ml.dl4j.model.SimpleMultiLayerNetwork;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Test for DL4JModelWriter actor.
- * <br>
- * NB: Dummy test.
  *
  * @author fracpete
  * @author adams.core.option.FlowJUnitTestProducer (code generator)
  * @version $Revision$
  */
 public class DL4JModelWriterTest
-  extends AbstractDL4JFlowTest {
+  extends AbstractFlowTest {
 
   /**
    * Initializes the test.
@@ -46,6 +55,28 @@ public class DL4JModelWriterTest
    */
   public DL4JModelWriterTest(String name) {
     super(name);
+  }
+
+  /**
+   * Called by JUnit before each test method.
+   *
+   * @throws Exception 	if an error occurs.
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    m_TestHelper.deleteFileFromTmp("simple.model");
+  }
+
+  /**
+   * Called by JUnit after each test method.
+   *
+   * @throws Exception	if tear-down fails
+   */
+  protected void tearDown() throws Exception {
+    m_TestHelper.deleteFileFromTmp("simple.model");
+
+    super.tearDown();
   }
 
   /**
@@ -64,7 +95,43 @@ public class DL4JModelWriterTest
    * @return a suitably configured <code>AbstractActor</code> value
    */
   public AbstractActor getActor() {
-    return new Flow();
+    AbstractArgumentOption    argOption;
+    
+    Flow flow = new Flow();
+    
+    try {
+      List<Actor> actors = new ArrayList<>();
+
+      // Flow.DL4JModelConfigurator
+      DL4JModelConfigurator dl4jmodelconfigurator = new DL4JModelConfigurator();
+      SimpleMultiLayerNetwork simplemultilayernetwork = new SimpleMultiLayerNetwork();
+      dl4jmodelconfigurator.setModelConfigurator(simplemultilayernetwork);
+
+      actors.add(dl4jmodelconfigurator);
+
+      // Flow.Convert
+      Convert convert = new Convert();
+      DL4JConfiguratorToModel dl4jconfiguratortomodel = new DL4JConfiguratorToModel();
+      convert.setConversion(dl4jconfiguratortomodel);
+
+      actors.add(convert);
+
+      // Flow.DL4JModelWriter
+      DL4JModelWriter dl4jmodelwriter = new DL4JModelWriter();
+      argOption = (AbstractArgumentOption) dl4jmodelwriter.getOptionManager().findByProperty("outputFile");
+      dl4jmodelwriter.setOutputFile((PlaceholderFile) argOption.valueOf("${TMP}/simple.model"));
+      actors.add(dl4jmodelwriter);
+      flow.setActors(actors.toArray(new Actor[0]));
+
+      NullListener nulllistener = new NullListener();
+      flow.setFlowExecutionListener(nulllistener);
+
+    }
+    catch (Exception e) {
+      fail("Failed to set up actor: " + e);
+    }
+    
+    return flow;
   }
 
   /**
@@ -73,7 +140,7 @@ public class DL4JModelWriterTest
    * @param args	ignored
    */
   public static void main(String[] args) {
-    Environment.setEnvironmentClass(Environment.class);
+    Environment.setEnvironmentClass(adams.env.Environment.class);
     runTest(suite());
   }
 }
