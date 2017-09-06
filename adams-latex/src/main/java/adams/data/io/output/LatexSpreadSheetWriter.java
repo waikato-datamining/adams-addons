ad98cfa3c627f4346532ccdd666b17d610b3ec6a
Peter Reutemann <fracpete@gmail.com>
2017-09-06 13:33:22 +1200

    allows bold face now for header

--- adams-latex/src/main/java/adams/data/io/output/LatexSpreadSheetWriter.java
+++ adams-latex/src/main/java/adams/data/io/output/LatexSpreadSheetWriter.java
@@ -13,9 +13,9 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
+/*
  * LatexSpreadSheetWriter.java
- * Copyright (C) 2011-2015 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2011-2017 University of Waikato, Hamilton, New Zealand
  */
 package adams.data.io.output;
 
@@ -34,13 +34,14 @@ import java.util.logging.Level;
  <!-- globalinfo-end -->
  *
  <!-- options-start -->
- * Valid options are: <br><br>
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
  *
- * <pre>-D &lt;int&gt; (property: debugLevel)
- * &nbsp;&nbsp;&nbsp;The greater the number the more additional info the scheme may output to
- * &nbsp;&nbsp;&nbsp;the console (0 = off).
- * &nbsp;&nbsp;&nbsp;default: 0
- * &nbsp;&nbsp;&nbsp;minimum: 0
+ * <pre>-sheet-prefix &lt;java.lang.String&gt; (property: sheetPrefix)
+ * &nbsp;&nbsp;&nbsp;The prefix for sheet names.
+ * &nbsp;&nbsp;&nbsp;default: Sheet
  * </pre>
  *
  * <pre>-missing &lt;java.lang.String&gt; (property: missingValue)
@@ -48,10 +49,20 @@ import java.util.logging.Level;
  * &nbsp;&nbsp;&nbsp;default:
  * </pre>
  *
+ * <pre>-locale &lt;java.util.Locale&gt; (property: locale)
+ * &nbsp;&nbsp;&nbsp;The locale to use for formatting the numbers.
+ * &nbsp;&nbsp;&nbsp;default: Default
+ * </pre>
+ *
  * <pre>-number-format &lt;java.lang.String&gt; (property: numberFormat)
  * &nbsp;&nbsp;&nbsp;The format for the numbers (see java.text.DecimalFormat), use empty string
  * &nbsp;&nbsp;&nbsp;for default 'double' output.
  * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-header-bold &lt;boolean&gt; (property: headerBold)
+ * &nbsp;&nbsp;&nbsp;If enabled, the header gets output in bold face.
+ * &nbsp;&nbsp;&nbsp;default: false
  * </pre>
  *
  <!-- options-end -->
@@ -65,6 +76,9 @@ public class LatexSpreadSheetWriter
   /** for serialization. */
   private static final long serialVersionUID = 8693302523602090616L;
 
+  /** whether to make header bold. */
+  protected boolean m_HeaderBold;
+
   /**
    * Returns a string describing the object.
    *
@@ -73,6 +87,47 @@ public class LatexSpreadSheetWriter
   @Override
   public String globalInfo() {
     return "Generates LaTeX tables from spreadsheets.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "header-bold", "headerBold",
+      false);
+  }
+
+  /**
+   * Sets whether to make the header bold.
+   *
+   * @param value	true if bold
+   */
+  public void setHeaderBold(boolean value) {
+    m_HeaderBold = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to make the header bold.
+   *
+   * @return 		true if bold
+   */
+  public boolean getHeaderBold() {
+    return m_HeaderBold;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String headerBoldTipText() {
+    return "If enabled, the header gets output in bold face.";
   }
 
   /**
@@ -98,7 +153,7 @@ public class LatexSpreadSheetWriter
 
   /**
    * Returns, if available, the corresponding reader.
-   * 
+   *
    * @return		the reader, null if none available
    */
   public SpreadSheetReader getCorrespondingReader() {
@@ -202,10 +257,14 @@ public class LatexSpreadSheetWriter
 	    writer.write(" & ");
 	  else
 	    writer.write("  ");
+	  if (m_HeaderBold)
+	    writer.write("\\textbf{");
 	  if (cell.isMissing())
 	    writer.write(escape(m_MissingValue));
 	  else
 	    writer.write(escape(cell.getContent()));
+	  if (m_HeaderBold)
+	    writer.write("}");
 
 	  first = false;
 	}
