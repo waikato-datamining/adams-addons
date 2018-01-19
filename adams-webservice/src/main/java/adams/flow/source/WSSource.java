500dec6cbf18c7ac1997e745826788ce185cbab7
Peter Reutemann <fracpete@gmail.com>
2018-01-19 14:43:42 +1300

    fixed return type of getDefaultClient method

--- adams-webservice/src/main/java/adams/flow/source/WSSource.java
+++ adams-webservice/src/main/java/adams/flow/source/WSSource.java
@@ -13,9 +13,9 @@
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
+/*
  * WSSource.java
- * Copyright (C) 2012-2014 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2012-2018 University of Waikato, Hamilton, New Zealand
  */
 package adams.flow.source;
 
@@ -23,9 +23,7 @@ import adams.flow.core.AbstractWSClient;
 import adams.flow.core.OutputProducer;
 import adams.flow.core.Token;
 import adams.flow.webservice.WebServiceClient;
-import adams.flow.webservice.WebServiceClientConsumer;
 import adams.flow.webservice.WebServiceClientProducer;
-
 import com.example.customerservice.flow.CustomersByName;
 
 /**
@@ -43,43 +41,42 @@ import com.example.customerservice.flow.CustomersByName;
  *
  <!-- options-start -->
  * Valid options are: <br><br>
- * 
+ *
  * <pre>-D &lt;int&gt; (property: debugLevel)
  * &nbsp;&nbsp;&nbsp;The greater the number the more additional info the scheme may output to 
  * &nbsp;&nbsp;&nbsp;the console (0 = off).
  * &nbsp;&nbsp;&nbsp;default: 0
  * &nbsp;&nbsp;&nbsp;minimum: 0
  * </pre>
- * 
+ *
  * <pre>-name &lt;java.lang.String&gt; (property: name)
  * &nbsp;&nbsp;&nbsp;The name of the actor.
  * &nbsp;&nbsp;&nbsp;default: WSSource
  * </pre>
- * 
+ *
  * <pre>-annotation &lt;adams.core.base.BaseText&gt; (property: annotations)
  * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
  * &nbsp;&nbsp;&nbsp;default: 
  * </pre>
- * 
+ *
  * <pre>-skip (property: skip)
  * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded 
  * &nbsp;&nbsp;&nbsp;as it is.
  * </pre>
- * 
+ *
  * <pre>-stop-flow-on-error (property: stopFlowOnError)
  * &nbsp;&nbsp;&nbsp;If set to true, the flow gets stopped in case this actor encounters an error;
  * &nbsp;&nbsp;&nbsp; useful for critical actors.
  * </pre>
- * 
+ *
  * <pre>-client &lt;adams.flow.webservice.WebServiceClient&gt; (property: client)
  * &nbsp;&nbsp;&nbsp;The webservice client to use.
  * &nbsp;&nbsp;&nbsp;default: com.example.customerservice.flow.CustomersByName
  * </pre>
- * 
+ *
  <!-- options-end -->
  *
  * @author  fracpete (fracpete at waikato dot ac dot nz)
- * @version $Revision$
  */
 public class WSSource
   extends AbstractWSClient
@@ -97,25 +94,25 @@ public class WSSource
   public String globalInfo() {
     return "Obtains data from a webservice and forwards it.";
   }
-  
+
   /**
    * Returns the default client to use.
-   * 
+   *
    * @return		the client
    */
   @Override
-	protected WebServiceClientConsumer getDefaultClient() {
+  protected WebServiceClientProducer getDefaultClient() {
     return new CustomersByName();
   }
-  
+
   /**
    * Checks the client.
-   * 
+   *
    * @param value	the client to check
    * @return		null if accepted, otherwise error message
    */
   @Override
-	protected String checkClient(WebServiceClient value) {
+  protected String checkClient(WebServiceClient value) {
     if (!(value instanceof WebServiceClientProducer))
       return "Does not implement " + WebServiceClientProducer.class.getName() + "!";
     return null;
@@ -161,7 +158,7 @@ public class WSSource
   public Token output() {
     return new Token(((WebServiceClientProducer) m_Client).getResponseData());
   }
-  
+
   /**
    * Cleans up the actor.
    */
