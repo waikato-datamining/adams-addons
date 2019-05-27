33fb92f7d79629018c6654c8b743d31ee44ca422
Peter Reutemann <fracpete@gmail.com>
2019-05-27 12:32:43 +1200

    moved locating of Key/TrustManagerFactoryProvider, SSLContextProvider into adams-core/TSLHelper, with TLSUtils derived from TLSHelper

--- adams-webservice-core/src/main/java/adams/flow/core/TLSUtils.java
+++ adams-webservice-core/src/main/java/adams/flow/core/TLSUtils.java
@@ -30,38 +30,10 @@ import org.apache.cxf.transport.http.HTTPConduit;
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
  */
-public class TLSUtils {
+public class TLSUtils
+  extends TLSHelper {
 
   public static final String FALLBACK_TLS_VERSION = "TLS";
-
-  /**
-   * Locates the following actors KeyManager (=0), TrustManager (=1), SSLContext (=2)
-   *
-   * @param context	the flow context
-   * @param requireAll 	whether all the actors need to be present
-   * @return		the actors, null if all required and not all were present
-   */
-  public static Struct3<KeyManagerFactoryProvider,TrustManagerFactoryProvider,SSLContextProvider> locateActors(Actor context, boolean requireAll) {
-    Struct3<KeyManagerFactoryProvider,TrustManagerFactoryProvider,SSLContextProvider>	result;
-    KeyManagerFactoryProvider 		keyManager;
-    TrustManagerFactoryProvider 	trustManager;
-    SSLContextProvider			sslContext;
-
-    keyManager   = (KeyManagerFactoryProvider) ActorUtils.findClosestType(context, KeyManagerFactoryProvider.class, true);
-    trustManager = (TrustManagerFactoryProvider) ActorUtils.findClosestType(context, TrustManagerFactoryProvider.class, true);
-    sslContext   = (SSLContextProvider) ActorUtils.findClosestType(context, SSLContextProvider.class, true);
-
-    if (requireAll) {
-      if ((keyManager == null) || (trustManager == null) || sslContext == null)
-        return null;
-      result = new Struct3<>(keyManager, trustManager, sslContext);
-    }
-    else {
-      result = new Struct3<>(keyManager, trustManager, sslContext);
-    }
-
-    return result;
-  }
 
   /**
    * Configures TLS client parameters based on KeyManager, TrustManager, SSLContext
