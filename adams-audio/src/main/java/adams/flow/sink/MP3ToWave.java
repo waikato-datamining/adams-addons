d86ca0103a50cd074c1af6937d6f2d257fefeaf1
Peter Reutemann <fracpete@gmail.com>
2020-06-28 13:43:12 +1200

    converter for MP3 to WAV: MP3ToWave

---
+++ adams-audio/src/main/java/adams/flow/sink/MP3ToWave.java
@@ -0,0 +1,197 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * MP3ToWave.java
+ * Copyright (C) 2020 University of Waikato, Hamilton, NZ
+ */
+
+package adams.flow.sink;
+
+import adams.core.QuickInfoHelper;
+import adams.core.io.FileWriter;
+import adams.core.io.PlaceholderFile;
+import javazoom.jl.converter.Converter;
+
+import java.io.File;
+
+/**
+ <!-- globalinfo-start -->
+ * Converts the incoming MP3 file into a Wave file (the output file).
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ *
+ * <pre>-name &lt;java.lang.String&gt; (property: name)
+ * &nbsp;&nbsp;&nbsp;The name of the actor.
+ * &nbsp;&nbsp;&nbsp;default: MP3ToWave
+ * </pre>
+ *
+ * <pre>-annotation &lt;adams.core.base.BaseAnnotation&gt; (property: annotations)
+ * &nbsp;&nbsp;&nbsp;The annotations to attach to this actor.
+ * &nbsp;&nbsp;&nbsp;default:
+ * </pre>
+ *
+ * <pre>-skip &lt;boolean&gt; (property: skip)
+ * &nbsp;&nbsp;&nbsp;If set to true, transformation is skipped and the input token is just forwarded
+ * &nbsp;&nbsp;&nbsp;as it is.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-stop-flow-on-error &lt;boolean&gt; (property: stopFlowOnError)
+ * &nbsp;&nbsp;&nbsp;If set to true, the flow execution at this level gets stopped in case this
+ * &nbsp;&nbsp;&nbsp;actor encounters an error; the error gets propagated; useful for critical
+ * &nbsp;&nbsp;&nbsp;actors.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-silent &lt;boolean&gt; (property: silent)
+ * &nbsp;&nbsp;&nbsp;If enabled, then no errors are output in the console; Note: the enclosing
+ * &nbsp;&nbsp;&nbsp;actor handler must have this enabled as well.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ *
+ * <pre>-output &lt;adams.core.io.PlaceholderFile&gt; (property: outputFile)
+ * &nbsp;&nbsp;&nbsp;The name of the Wave output file.
+ * &nbsp;&nbsp;&nbsp;default: ${CWD}
+ * </pre>
+ *
+ <!-- options-end -->
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class MP3ToWave
+  extends AbstractSink
+  implements FileWriter {
+
+  private static final long serialVersionUID = 8441244538728070082L;
+
+  /** the output file. */
+  protected PlaceholderFile m_OutputFile;
+
+  /**
+   * Returns a string describing the object.
+   *
+   * @return 			a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Converts the incoming MP3 file into a Wave file (the output file).";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  @Override
+  public void defineOptions() {
+    super.defineOptions();
+
+    m_OptionManager.add(
+      "output", "outputFile",
+      getDefaultOutputFile());
+  }
+
+  /**
+   * Returns the default output file.
+   *
+   * @return		the file
+   */
+  protected PlaceholderFile getDefaultOutputFile() {
+    return new PlaceholderFile(".");
+  }
+
+  /**
+   * Returns a quick info about the actor, which will be displayed in the GUI.
+   *
+   * @return		null if no info available, otherwise short string
+   */
+  @Override
+  public String getQuickInfo() {
+    return QuickInfoHelper.toString(this, "outputFile", m_OutputFile);
+  }
+
+  /**
+   * Set wave output file.
+   *
+   * @param value	file
+   */
+  public void setOutputFile(PlaceholderFile value) {
+    m_OutputFile = value;
+    reset();
+  }
+
+  /**
+   * Get wave output file.
+   *
+   * @return	file
+   */
+  public PlaceholderFile getOutputFile() {
+    return m_OutputFile;
+  }
+
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String outputFileTipText() {
+    return "The name of the Wave output file.";
+  }
+
+  /**
+   * Returns the class that the consumer accepts.
+   *
+   * @return		the Class of objects that can be processed
+   */
+  @Override
+  public Class[] accepts() {
+    return new Class[]{String.class, File.class};
+  }
+
+  /**
+   * Executes the flow item.
+   *
+   * @return		null if everything is fine, otherwise error message
+   */
+  @Override
+  protected String doExecute() {
+    String	result;
+    String	filename;
+    Converter	converter;
+
+    result = null;
+
+    if (m_InputToken.hasPayload(File.class))
+      filename = m_InputToken.getPayload(File.class).getAbsolutePath();
+    else
+      filename = m_InputToken.getPayload(String.class);
+
+    try {
+      converter = new Converter();
+      converter.convert(new PlaceholderFile(filename).getAbsolutePath(), m_OutputFile.getAbsolutePath());
+    }
+    catch (Exception e) {
+      result = handleException("Failed to convert '" + filename + "' to '" + m_OutputFile + "'!", e);
+    }
+
+    return result;
+  }
+}
