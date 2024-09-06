708cd03bea2d584329963749ca85b79546b9ae16
Peter Reutemann <fracpete@gmail.com>
2024-09-06 14:58:24 +1200

    updated copyright

--- adams-terminal/src/main/java/adams/terminal/menu/About.java
+++ adams-terminal/src/main/java/adams/terminal/menu/About.java
@@ -15,7 +15,7 @@
 
 /*
  * About.java
- * Copyright (C) 2016-2023 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2016-2024 University of Waikato, Hamilton, New Zealand
  *
  */
 
@@ -62,7 +62,7 @@ public class About
       context,
       getTitle(),
       "Advanced Data Mining and Machine Learning System\n"
-	+ "(c) 2009-2023 University of Waikato\n"
+	+ "(c) 2009-2024 University of Waikato\n"
 	+ "Hamilton, New Zealand");
   }
 
