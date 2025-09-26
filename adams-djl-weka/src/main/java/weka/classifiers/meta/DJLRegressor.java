e512ea58bb079edabb0e47f4d69fd4a33b2b6510
Peter Reutemann <fracpete@gmail.com>
2025-09-26 16:00:44 +1200

    now implements AutoCloseable; keeping track of models via their path/ID and close them if necessary; deleting existing .params files now properly

--- adams-djl-weka/src/main/java/weka/classifiers/meta/DJLRegressor.java
+++ adams-djl-weka/src/main/java/weka/classifiers/meta/DJLRegressor.java
@@ -49,8 +49,11 @@ import weka.core.Instances;
 import weka.core.Utils;
 import weka.djl.InstancesDataset;
 
+import java.io.Closeable;
 import java.io.File;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Uses Deep Java Library for building a regression model.
@@ -59,9 +62,12 @@ import java.nio.file.Path;
  */
 public class DJLRegressor
   extends AbstractSimpleClassifier
-  implements ScriptedClassifier {
+  implements ScriptedClassifier, AutoCloseable {
 
   private static final long serialVersionUID = -8361229968357782660L;
+
+  /** for keeping track of models. */
+  protected static Map<String,Model> m_Models = new HashMap<>();
 
   /** the network generator to use. */
   protected NetworkGenerator m_Network;
@@ -382,6 +388,7 @@ public class DJLRegressor
     String 				modelID;
     File				modelDir;
     Path 				modelPath;
+    String				modelName;
 
     getCapabilities().test(data);
     m_Network.setFlowContext(m_FlowContext);
@@ -389,14 +396,23 @@ public class DJLRegressor
     m_ID.setFlowContext(m_FlowContext);
 
     modelID   = m_ID.generate();
-    modelDir  = m_OutputDir.generate();
+    modelDir  = m_OutputDir.generate().getAbsoluteFile();
     modelPath = Path.of(modelDir.getAbsolutePath());
+    modelName = modelDir + "|" + modelID;
 
     // delete any left-over .params files
     for (File f: modelDir.listFiles()) {
       if (f.getName().matches("^" + modelID + "-[0-9]+.params$")) {
 	if (isLoggingEnabled())
 	  getLogger().info("Removing: " + f);
+	try {
+	  if (!f.delete())
+	    System.err.println("Failed to delete: " + f);
+	}
+	catch (Exception e) {
+	  System.err.println("Failed to delete: " + f);
+	  e.printStackTrace();
+	}
       }
     }
 
@@ -414,8 +430,16 @@ public class DJLRegressor
     trainDataset    = splitDataset[0];
     validateDataset = splitDataset[1];
 
-    m_Model = Model.newInstance("tabular");
-    m_Model.setBlock(m_Network.generate(m_Dataset));
+
+    synchronized (m_Models) {
+      if (m_Models.containsKey(modelName)) {
+	m_Models.get(modelName).close();
+	m_Models.remove(modelName);
+      }
+      m_Model = Model.newInstance(modelName);
+      m_Model.setBlock(m_Network.generate(m_Dataset));
+      m_Models.put(modelName, m_Model);
+    }
 
     trainingConfig = new DefaultTrainingConfig(
       new TabNetRegressionLoss())
@@ -537,5 +561,63 @@ public class DJLRegressor
    */
   public static void main(String[] args) throws Exception {
     runClassifier(new DJLRegressor(), args);
+  }
+
+  /**
+   * Closes this resource, relinquishing any underlying resources.
+   * This method is invoked automatically on objects managed by the
+   * {@code try}-with-resources statement.
+   *
+   * <p>While this interface method is declared to throw {@code
+   * Exception}, implementers are <em>strongly</em> encouraged to
+   * declare concrete implementations of the {@code close} method to
+   * throw more specific exceptions, or to throw no exception at all
+   * if the close operation cannot fail.
+   *
+   * <p> Cases where the close operation may fail require careful
+   * attention by implementers. It is strongly advised to relinquish
+   * the underlying resources and to internally <em>mark</em> the
+   * resource as closed, prior to throwing the exception. The {@code
+   * close} method is unlikely to be invoked more than once and so
+   * this ensures that the resources are released in a timely manner.
+   * Furthermore it reduces problems that could arise when the resource
+   * wraps, or is wrapped, by another resource.
+   *
+   * <p><em>Implementers of this interface are also strongly advised
+   * to not have the {@code close} method throw {@link
+   * InterruptedException}.</em>
+   * <p>
+   * This exception interacts with a thread's interrupted status,
+   * and runtime misbehavior is likely to occur if an {@code
+   * InterruptedException} is {@linkplain Throwable#addSuppressed
+   * suppressed}.
+   * <p>
+   * More generally, if it would cause problems for an
+   * exception to be suppressed, the {@code AutoCloseable.close}
+   * method should not throw it.
+   *
+   * <p>Note that unlike the {@link Closeable#close close}
+   * method of {@link Closeable}, this {@code close} method
+   * is <em>not</em> required to be idempotent.  In other words,
+   * calling this {@code close} method more than once may have some
+   * visible side effect, unlike {@code Closeable.close} which is
+   * required to have no effect if called more than once.
+   * <p>
+   * However, implementers of this interface are strongly encouraged
+   * to make their {@code close} methods idempotent.
+   *
+   * @throws Exception if this resource cannot be closed
+   */
+  @Override
+  public void close() throws Exception {
+    if (m_Model != null) {
+      try {
+	m_Model.close();
+      }
+      catch (Exception e) {
+	// ignored
+      }
+      m_Model = null;
+    }
   }
 }
